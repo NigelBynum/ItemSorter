94a07611d5dbb328cd1b27aa6f43e80dbbd550ff
nigel <bynum1001@gmail.com>
2023-07-26 09:45:37 -0400

    Complete

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,40 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
-    public Item() {
+   private Double price;
+   private String name;
+   private Long id;
+
+   public Item() {
+        this.price = 0.01;
+        this.name= "";
+        this.id= 0L;
     }
 
     public Item(Long id, String name, Double price) {
+        this.price = price;
+        this.name= name;
+        this.id= id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
-    public void setId(Long id) {
-    }
+    public void setId(Long id) {this.id = id;}
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(Double price) { this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,71 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+
+    public class IdComparator implements Comparator<Item> {
+
+        @Override
+        public int compare(Item o1, Item o2) {
+            return o1.getId().compareTo(o2.getId());
+        }
+    }
+
+
+
+    //The Comparator interface has a single method called compare(), which takes two objects of the same type and returns an integer value based on their comparison. The compare() method has the following signature:
+   // int compare(T o1, T o2);
+    /*If compare() returns a negative value: It means o1 is less than o2.
+        If compare() returns zero: It means o1 is equal to o2.
+        If compare() returns a positive value: It means o1 is greater than o2.*/
+
+/*class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 25));
+        people.add(new Person("Bob", 30));
+        people.add(new Person("Charlie", 20));
+
+        // Sorting the list based on age using a custom Comparator
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return p1.getAge() - p2.getAge();
+            }
+        });
+
+        // Displaying the sorted list
+        for (Person person : people) {
+            System.out.println(person.getName() + " - " + person.getAge());
+        }
+    }
+}
+
+output
+        Charlie - 20
+        Alice - 25
+        Bob - 30*/
+
